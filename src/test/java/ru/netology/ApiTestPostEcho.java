c3ff4dcd4821ca81bc9991fe37bfd0be3196fb69
Elena <elenazhilina@yandex.ru>
2024-05-09 12:22:59 +0500

    Initial Commit

---
+++ src/test/java/ru/netology/ApiTestPostEcho.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class ApiTestPostEcho {
+
+    @Test
+            void testPostEcho() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("example data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("example data"))
+        ;
+    }
+}
